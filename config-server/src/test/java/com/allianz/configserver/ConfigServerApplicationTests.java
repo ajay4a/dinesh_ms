6129d8b2c5d2d847a54f82a63bee4f355c219e8a
ajay vishwakarma <ajay4a@gmail.com>
2023-07-17 19:51:15 +0530

    Added configserver.

---
+++ config-server/src/test/java/com/allianz/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
